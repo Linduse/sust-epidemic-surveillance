05ba1a1f9d6a7ea149e2840c4838c435a73849bb
zero <1444171773@qq.com>
2021-11-03 00:06:13 +0800

    feat(global): 出行指南

--- pages/community/community.js
+++ pages/community/community.js
@@ -5,9 +5,45 @@ Page({
    * 页面的初始数据
    */
   data: {
-
+      fromColor:"green",
+      toColor:"green",
+      fromText:"全部地区低风险",
+      toText:"全部地区低风险",
+      cityPolicy:{},
+      multiIndexFrom:[0,0],
+      multiIndexTo:[],
+      multiArray:[['北京市','天津市','上海市','重庆市'],['北京市','朝阳区']],
+      // 弹出层
+      show: false,
+      duration: 300,
+      position: 'bottom',
+      round: true,
+      overlay: true,
+      customStyle: 'height: 40%;',
+      overlayStyle: ''
   },
-
+  //选择器改变
+  fromBindMultiPickerChange: function (e) {
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      multiIndexFrom: e.detail.value
+    })
+  },
+  toBindMultiPickerChange: function (e) {
+    console.log(this.data.multiIndexTo.length)
+    console.log('picker发送选择改变，携带值为', e.detail.value)
+    this.setData({
+      multiIndexTo: e.detail.value
+    })
+    console.log(this.data.multiIndexTo.length)
+  },
+  //点击弹出遮罩层
+  popup(e) {
+    console.log("弹出一层")
+    this.setData({
+      show: true,
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
